627831ee2ba6666ce4e1f009e1dc5d9e1565e14c
bensouici akram <bensouiciakram@gmail.com>
2025-10-13 14:25:03 +0100

    changing the backend url envirement variable name

--- lib/http/client.ts
+++ lib/http/client.ts
@@ -4,7 +4,8 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_BASE_URL =
+  process.env.NEXT_PRICE_ALERT_PUBLIC_API_URL || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
